fca19e3ec5916684c5cfe63f0abed3f19761478c
unmuntean <59424779+unmuntean@users.noreply.github.com>
2023-04-07 13:36:15 +0300

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,7 +27,7 @@ export default function Home() {
   }>({
     messages: [
       {
-        message: 'Hi, what would you like to learn about this legal case?',
+        message: 'Salutari, eu sunt asistentul inteligent . Cu ce te pot ajuta?',
         type: 'apiMessage',
       },
     ],
@@ -51,7 +51,7 @@ export default function Home() {
     setError(null);
 
     if (!query) {
-      alert('Please input a question');
+      alert('Intreaba-ma orice..');
       return;
     }
 
@@ -165,7 +165,7 @@ export default function Home() {
       <Layout>
         <div className="mx-auto flex flex-col gap-4">
           <h1 className="text-2xl font-bold leading-[1.1] tracking-tighter text-center">
-            Chat With Your Legal Docs
+            Asistentul inteligent al produsului tau.
           </h1>
           <main className={styles.main}>
             <div className={styles.cloud}>
@@ -282,7 +282,7 @@ export default function Home() {
                     placeholder={
                       loading
                         ? 'Waiting for response...'
-                        : 'What is this legal case about?'
+                        : 'Intreaba-ma orice...'
                     }
                     value={query}
                     onChange={(e) => setQuery(e.target.value)}
@@ -320,7 +320,7 @@ export default function Home() {
         </div>
         <footer className="m-auto p-4">
           <a href="https://twitter.com/mayowaoshin">
-            Powered by LangChainAI. Demo built by Mayo (Twitter: @mayowaoshin).
+            Powered by GuidefAI AIDoc v1.0.
           </a>
         </footer>
       </Layout>
--- utils/makechain.ts
+++ utils/makechain.ts
@@ -5,7 +5,7 @@ import { PromptTemplate } from 'langchain/prompts';
 import { CallbackManager } from 'langchain/callbacks';
 
 const CONDENSE_PROMPT =
-  PromptTemplate.fromTemplate(`Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+  PromptTemplate.fromTemplate(`Avand in vedere urmatoarea conversatie si o intrebare ulterioara, reformulati intrebarea ulterioara pentru a fi o intrebare de sine statatoare.
 
 Chat History:
 {chat_history}
@@ -13,10 +13,7 @@ Follow Up Input: {question}
 Standalone question:`);
 
 const QA_PROMPT = PromptTemplate.fromTemplate(
-  `You are an AI assistant providing helpful advice. You are given the following extracted parts of a long document and a question. Provide a conversational answer based on the context provided.
-You should only provide hyperlinks that reference the context below. Do NOT make up hyperlinks.
-If you can't find the answer in the context below, just say "Hmm, I'm not sure." Don't try to make up an answer.
-If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
+  `Vorbeste in dialectul sesizat in document. Foloseste acelasi stil de a expune mesajul. Fi cat mai la scurt si la subiect.Concis. Esti un expertul unui cabinet de avocatura si scopul tau este sa ajuti avocatul sa inteleaga rapid si clar legile,articolele si MAI ALES cum poate sa-si salveze clientii in instanta.
 
 Question: {question}
 =========
@@ -36,7 +33,7 @@ export const makeChain = (
   const docChain = loadQAChain(
     new OpenAIChat({
       temperature: 0,
-      modelName: 'gpt-4', //change this to older versions (e.g. gpt-3.5-turbo) if you don't have access to gpt-4
+      modelName: 'gpt-3.5-turbo', //change this to older versions (e.g. gpt-3.5-turbo) if you don't have access to gpt-4
       streaming: Boolean(onTokenStream),
       callbackManager: onTokenStream
         ? CallbackManager.fromHandlers({
